481756de0c8e452676b9ae8ea1b4ec4fe005644e
Joseph Kuo <jovesus@080-115.eduroam.rwth-aachen.de>
2019-10-21 14:42:48 +0200

    modified:   data/setup_genome_data.py 	modified:   setup.py

--- data/setup_genome_data.py
+++ data/setup_genome_data.py
@@ -1,11 +1,86 @@
-import argparse
+#!/usr/bin/env python
+"""
+Usage:
+    setup_genome_data.py mm9 [--fasta] [--gtf]
+    setup_genome_data.py mm10 [--fasta] [--gtf]
+    setup_genome_data.py hg19 [--fasta] [--gtf]
+    setup_genome_data.py hg38 [--fasta] [--gtf]
 
-parser = argparse.ArgumentParser(description='Setup the genome data under the defined directory.')
-parser.add_argument('-g','--genome', dest='genome', type=str, help='the given genome name (such as hg38, mm10...).')
-parser.add_argument('-a', '--all', dest='all', action='store_true', default=False,
-                    help='Download both the genome (FASTA) and annotation (GTF). (default: False)')
-parser.add_argument('--fasta', dest='fasta', action='store_true', default=False,
-                    help='Download the genome (FASTA). (default: False)')
+Download the latest genome FASTA file or annotation GTF according to the given genome.
 
-args = parser.parse_args()
-print(args.accumulate(args.integers))
+Options:
+    --fasta                  Download the genome FASTA file [default: False]
+    --gtf                    Download the annotation GTF file [default: False]
+"""
+from docopt import docopt
+import sys
+import os
+import gzip
+import ftplib
+from progressbar import ProgressBar, Percentage, Bar, ETA, FileTransferSpeed
+from tqdm import tqdm
+import urllib.request
+from contextlib import closing
+
+latest_fasta = {
+"mm9":"ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M1/NCBIM37.genome.fa.gz",
+"mm10":"ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M23/GRCm38.primary_assembly.genome.fa.gz",
+"hg19":"ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_19/GRCh37.p13.genome.fa.gz",
+"hg38":"ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_32/GRCh38.primary_assembly.genome.fa.gz"
+}
+latest_gtf = {
+"mm9":"ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M1/gencode.vM1.annotation.gtf.gz",
+"mm10":"ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M23/gencode.vM23.annotation.gtf.gz",
+"hg19":"ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_19/gencode.v19.annotation.gtf.gz",
+"hg38":"ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_32/gencode.v32.annotation.gtf.gz"
+}
+
+
+class TqdmUpTo(tqdm):
+    """Provides `update_to(n)` which uses `tqdm.update(delta_n)`."""
+
+    def update_to(self, b=1, bsize=1, tsize=None):
+        """
+        b  : int, optional
+            Number of blocks transferred so far [default: 1].
+        bsize  : int, optional
+            Size of each block (in tqdm units) [default: 1].
+        tsize  : int, optional
+            Total size (in tqdm units). If [default: None] remains unchanged.
+        """
+        if tsize is not None:
+            self.total = tsize
+        self.update(b * bsize - self.n)  # will also set self.n = b * bsize
+
+
+def download(url, target_dir):
+    base_name = os.path.basename(url).replace(".gz", "")
+    tmp_file = os.path.join(target_dir, base_name+".gz")
+    goal_file = os.path.join(target_dir, base_name)
+
+    eg_link = url
+    with TqdmUpTo(unit='B', unit_scale=True, miniters=1,
+                  desc=tmp_file) as t:  # all optional kwargs
+        urllib.request.urlretrieve(url, filename=tmp_file,
+                           reporthook=t.update_to, data=None)
+    # Unzip the file
+    gzfile = gzip.GzipFile(tmp_file, 'rb')
+    s = gzfile.read()
+    gzfile.close()
+    output = open(goal_file, 'wb')
+    output.write(s)
+    output.close()
+    os.remove(tmp_file)
+
+
+if __name__ == '__main__':
+    arg = docopt(__doc__)
+    genome = ""
+    for gg in latest_fasta.keys():
+        if arg[gg]:
+            genome = gg
+
+    if arg['--fasta']:
+        download(latest_fasta[genome], genome)
+    if arg["--gtf"]:
+        download(latest_gtf[genome], genome)
--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import os
 import re
 from sys import platform
 from Cython.Build import cythonize
+from setuptools.command.install import install
 
 """
 Installs gencoor with standard setuptools options.
@@ -27,6 +28,69 @@ def find_version(*file_paths):
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
 
+class CustomInstallCommand(install):
+    """Customized setuptools install command."""
+
+    def write_genome_data(self, config, genome, latest_GTFs, genome_organism):
+        config.write("[" + genome + "]\n")
+        config.write("genome: " + os.path.join(genome, "genome_" + genome + ".fa\n"))
+        config.write("chromosome_sizes: " + os.path.join(genome, "chrom.sizes." + genome + "\n"))
+        config.write("genes_Gencode: " + os.path.join(genome, "genes_" + genome + ".bed\n"))
+        config.write("annotation: " + os.path.join(genome, latest_GTFs[genome] + "\n"))
+        config.write("gene_alias: " + os.path.join(genome, "alias_" + genome_organism[genome] + ".txt\n\n"))
+
+    def run(self):
+        ###################################################################################################
+        # Creating Data Path
+        ###################################################################################################
+        latest_GTFs = {"mm9": "gencode.vM1.annotation.gtf",
+                       "mm10": "gencode.vM23.annotation.gtf",
+                       "hg19": "gencode.v19.annotation.gtf",
+                       "hg38": "gencode.v32.annotation.gtf"}
+        genome_organism = {"mm9": "mouse",
+                           "mm10": "mouse",
+                           "hg19": "human",
+                           "hg38": "human"}
+
+        # if the environment variable is set, use it; otherwise use the home directory as a default
+        gencoordata_location = os.path.expanduser(
+            os.getenv("GENCOORDATA", os.path.join(os.getenv("HOME"), "gencoor_data")))
+
+        # Creating Data Path
+        if not os.path.exists(gencoordata_location):
+            os.makedirs(gencoordata_location)
+
+        # Creating data.config
+        data_config_file_name = os.path.join(gencoordata_location, "data.config")
+        # if not os.path.isfile(data_config_file_name):
+        data_config_file = open(data_config_file_name, "w")
+        data_config_file.write(
+            "# Configuration file loaded at rgt startup. CAREFUL: any changes shall be overwritten\n"
+            "# whenever rgt is (re)installed. Use data.config.user for permanent changes.\n\n")
+        for genome in latest_GTFs.keys():
+            self.write_genome_data(data_config_file, genome, latest_GTFs, genome_organism)
+        data_config_file.close()
+
+        # Creating data.config.user, but only if not already present
+        user_config_file_name = os.path.join(gencoordata_location, "data.config.user")
+        if not os.path.isfile(user_config_file_name):
+            user_config_file = open(user_config_file_name, "w")
+
+            user_config_file.write(
+                "# Here you can overwrite any property set in the data.config file. It shall not be\n"
+                "# be overwritten in any case, so if you are experiencing problems rename or remove this\n"
+                "# file. See data.config for how the file should be formatted.\n\n")
+            genome = "self_defined"
+            user_config_file.write("# Template to add a genomic section.\n")
+            user_config_file.write("#[" + genome + "]\n")
+            user_config_file.write("#genome: undefined\n")
+            user_config_file.write("#chromosome_sizes: undefined\n")
+            user_config_file.write("#gene_regions: undefined\n")
+            user_config_file.write("#annotation: undefined\n")
+            user_config_file.write("#gene_alias: undefined\n\n")
+
+        install.run(self)
+
 current_version = find_version("gencoor", "__version__.py")
 
 ###################################################################################################
@@ -38,6 +102,8 @@ if platform not in supported_platforms:
     print("ERROR: This package currently supports only unix-based systems (Linux and MAC OS X).")
     exit(0)
 
+
+#############################
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -57,5 +123,6 @@ setuptools.setup(
                  "Operating System :: OS Independent"],
     ext_modules=cythonize("gencoor/coordinates.pyx",
                           compiler_directives={'language_level': "3"},
-                          annotate=True)
+                          annotate=True),
+    cmdclass={'install': CustomInstallCommand}
 )
